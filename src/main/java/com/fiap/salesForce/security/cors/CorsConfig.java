e5966bd9fd674aaae951d6b493303549385e6bc3
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-05-20 00:52:33 -0300

    remove credentials in CorsConfig

--- src/main/java/com/fiap/salesForce/security/cors/CorsConfig.java
+++ src/main/java/com/fiap/salesForce/security/cors/CorsConfig.java
@@ -11,7 +11,6 @@ public class CorsConfig implements WebMvcConfigurer {
         registry.addMapping("/**")
                 .allowedOrigins("*")
                 .allowedMethods("*")
-                .allowedHeaders("*")
-                .allowCredentials(true);
+                .allowedHeaders("*");
     }
 }
